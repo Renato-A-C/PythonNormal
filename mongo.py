c0717e4a48593a7a5adb6d777e6771e6493e9f81
GH0ST1NN3T <renatocoutinho10@gmail.com>
2023-06-19 00:52:50 -0300

    Att
    de rotina

---
+++ mongo.py
@@ -0,0 +1,22 @@
+import pymongo
+from pymongo import MongoClient
+import pprint
+client = MongoClient("mongodb://localhost:27017/")
+db = client.fanap
+post = {
+    "matricula":26,
+    "nome":"ARRROZZOMONSOAHJOISJOJOIJ",
+    "Curso":"pymongo",
+    "turno":"Matutino",
+    "periodo":8,
+    "bolsa":"nao",
+    "primeira_graduaçao":"sim",
+    "idade":41,
+    "necessidade_especial":"nao"    
+}
+fanap = db.fanap
+post_id = fanap.insert_one(post).inserted_id
+post_id
+db.list_collection_names()
+['fanap']
+pprint.pprint(fanap.find_one())
