1ffe23d81de3ff46da9ed08659d64aed0cb4278e
GH0ST1NN3T <renatocoutinho10@gmail.com>
2023-11-27 00:20:21 -0300

    Redução
    AAAAAAAAAAAAA

--- Dejango/djangoBasico/produtos/urls.py
+++ Dejango/djangoBasico/produtos/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+# produtos/
 urlpatterns = [
-    path('', views.index ),
-    path('celulares/', views.celulares ),
+    path('', views.index),
+    path('celulares/', views.celulares),
 ]
