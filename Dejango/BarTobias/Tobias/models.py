9ec8eabe4405dcb68bd30c2060395ac5c4233a6a
GH0ST1NN3T <renatocoutinho10@gmail.com>
2024-03-27 16:50:55 -0300

    att funcionario
    adsasdads

--- Dejango/BarTobias/Tobias/models.py
+++ Dejango/BarTobias/Tobias/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.postgres.functions import TransactionNow
+from django.contrib.auth.models import AbstractBaseUser
 # Create your models here.
 class Produto(models.Model):
     Nome_do_produto = models.CharField(db_column='nomeProduto', max_length = 30, blank=True)
@@ -9,10 +10,10 @@ class Produto(models.Model):
     Numero_de_Nota_Fiscal = models.CharField(db_column= 'NumNotaFiscal', max_length = 1000,blank = True)
     Quantidade_de_Produtos = models.BigIntegerField(db_column='Qtd', blank=True, default=1)
 
-
-
 class Cliente(models.Model):
     nome= models.CharField(db_column='nomeCliente', max_length=50, blank=False)
     cpf = models.CharField(db_column='cpfCliente', max_length=15, blank=True)
     cnpj = models.CharField(db_column='cnpjCliente', max_length=50, blank=True)
-    
+
+class User(AbstractBaseUser):
+    pass
