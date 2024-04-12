ea8fcefd482deee398500ada1a3489cb9fb9f089
GH0ST1NN3T <renatocoutinho10@gmail.com>
2024-04-12 17:45:51 -0300

    att
    Mk2.algumacoisa

--- Dejango/BarTobias/BarTobias/settings.py
+++ Dejango/BarTobias/BarTobias/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Tobias.apps.TobiasConfig'
+    'Tobias.apps.TobiasConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
     
 ]
 
@@ -52,6 +54,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'BarTobias.urls'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 TEMPLATES = [
     {
--- Dejango/BarTobias/Tobias/forms.py
+++ Dejango/BarTobias/Tobias/forms.py
@@ -1,44 +1,32 @@
 from django import forms
 from django.forms import Form, ModelForm
 from .models import Produto, Cliente, Venda, Funcionario
-
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 class ProdutoForm(ModelForm):
-    
     class Meta:
         model = Produto
         fields = '__all__'  
         labels = {
-            
-            
         }
+        
 class ClienteForm(ModelForm):
-    
     class Meta:
         model = Cliente
         fields = '__all__'  
         labels = {
-            
-            
         }
 
 class FuncionarioForm(ModelForm):
-    
     class Meta:
         model = Funcionario
         fields = '__all__'  
         labels = {
-            
-            
         }
         
-
 class VendaForm(ModelForm):
-    
     class Meta:
         model = Venda
         fields = '__all__'  
         labels = {
-            
-            
         }
