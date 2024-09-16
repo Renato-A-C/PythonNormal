c24ee71bdb81bb4afcc686ecc93e2aa5bf0b530f
Renato AC (GH0ST1NN3T) <renatocoutinho10@gmail.com>
2024-09-16 10:00:48 -0300

    Organizando
    brrrr

--- Dejango/BarTobias/Tobias/forms.py
+++
@@ -1,9 +0,0 @@
-
-from django.forms import ModelForm
-from .models import Produto
-
-class ProdutoForm(ModelForm):
-    class Meta:
-        model = Produto
-        fields = '__all__' 
-        print(fields)
--- Dejango/alte/loguin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LoguinConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'loguin'
